326121ba1055d23078911962f0ac4c01dc1d7c88
Anshuman <anshu5074@gmail.com>
2018-11-08 07:56:29 +0530

    Added report.jsx as per specs

--- report.jsx
+++ report.jsx
@@ -1,11 +1,78 @@
-var React = require('react')
-var ReactPivot = require('react-pivot')
-var createReactClass = require('create-react-class')
+var React = require('react');
+var ReactPivot = require('react-pivot');
+var createReactClass = require('create-react-class');
 
-var rows = require('./data.json')
+var rows = require('./data.json');
+
+var dimensions = [
+  {
+    value: 'date',
+    title: 'Date',
+  },
+  {
+    value: 'host',
+    title: 'Host',
+  },
+];
+
+var reduce = function(row, memo) {
+  memo.impressions =
+    (memo.impressions || 0) + parseInt(row['type'] === 'impression' ? 1 : 0);
+  memo.loads = (memo.loads || 0) + parseInt(row['type'] === 'load' ? 1 : 0);
+  memo.displays =
+    (memo.displays || 0) + parseInt(row['type'] === 'display' ? 1 : 0);
+  memo.loadRate = parseFloat(memo.loads / memo.impressions) * 100;
+  memo.displayRate = parseFloat(memo.displays / memo.loads) * 100;
+  return memo;
+};
+
+var calculations = [
+  {
+    title: 'Impressions',
+    value: 'impressions',
+    template: function(val, row) {
+      return val;
+    },
+  },
+  {
+    title: 'Loads',
+    value: 'loads',
+    template: function(val, row) {
+      return val;
+    },
+  },
+  {
+    title: 'Displays',
+    value: 'displays',
+    template: function(val, row) {
+      return val;
+    },
+  },
+  {
+    title: 'Load Rate',
+    value: 'loadRate',
+    template: function(val, row) {
+      return val.toFixed(1) + '%';
+    },
+  },
+  {
+    title: 'Display Rate',
+    value: 'displayRate',
+    template: function(val, row) {
+      return val.toFixed(1) + '%';
+    },
+  },
+];
 
 module.exports = createReactClass({
-  render () {
-    return <div>Report</div>
-  }
-})
+  render() {
+    return (
+      <ReactPivot
+        rows={rows}
+        reduce={reduce}
+        calculations={calculations}
+        dimensions={dimensions}
+      />
+    );
+  },
+});
